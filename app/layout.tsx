3ebd9add2b9fc276f07fe368afaa2ecc783f2d39
brwanda7 <b.kingsharoon@gmail.com>
2026-08-28 09:58:52 +0200

    pushing main micro finance

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.variable}>
-      <body class="flex min-h-screen flex-col">
+      <body className="flex min-h-screen flex-col">
         <Navbar />
-        <main class="flex-1">
-    <main class="relative min-h-screen">{children}</main>
+        <main className="flex-1">
+    <main className="relative min-h-screen">{children}</main>
     </main>
         <Footer />
       </body>
